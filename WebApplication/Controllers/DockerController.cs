ee1825d9b194632d2e64cbe59415c1427babd8ac
Pugach Alexander <fotone97@yandex.ru>
2020-10-26 21:54:03 +0300

    Add project files.

---
+++ WebApplication/Controllers/DockerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class DockerController : ControllerBase
+    {
+        private readonly ILogger<DockerController> _logger;
+
+        public DockerController(ILogger<DockerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return $"Hello wolrd from Docker Container: {Environment.MachineName}";
+            //return Environment.MachineName;
+        }
+    }
+}
